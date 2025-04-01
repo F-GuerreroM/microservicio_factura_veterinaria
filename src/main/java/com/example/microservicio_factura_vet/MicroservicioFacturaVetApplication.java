5dfa83433a842e7c924ecb9ecf93aacb33125ff6
Felipe <fel.guerrerom@duocuc.cl>
2025-03-31 22:25:12 -0300

    Inicio Repo

---
+++ src/main/java/com/example/microservicio_factura_vet/MicroservicioFacturaVetApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_factura_vet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioFacturaVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioFacturaVetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservicio_factura_vet/MicroservicioFacturaVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_factura_vet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioFacturaVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
